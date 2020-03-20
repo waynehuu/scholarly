a23d95d6364023439d4c1f4103cfb1ccc600e1ef
Wei (Wayne) Hu <wei.hu@duke.edu>
2020-03-20 15:43:23 -0400

    Add print energy term

--- main.py
+++ main.py
@@ -5,17 +5,17 @@ import time
 import random
 
 
-year_since = 2019  # Format: YYYY
+year_since = 2015  # Format: YYYY
 year_to = None  # Format: year_to should be no less than year_since
 
 result_items = 20
 
 energy_terms = [
-    'Wind',
+    # 'Wind',
     'Solar',
-    'Power system',
-    'Energy',
-    'Generator',
+    # 'Power system',
+    # 'Energy',
+    # 'Generator',
     # 'Coal',
     # 'Oil',
     # 'Natural Gas',
@@ -67,11 +67,13 @@ def make_url(kw, year_since, year_to):
 
     return url
 
-for e in tqdm(energy_terms):
+for e in energy_terms:
+    print(f'Scraping for energy term: {e}')
     results = []
-    for m in ml_terms:
+    for m in tqdm(ml_terms):
         for r in rs_terms:
             kw = '+'.join([quote(e), quote(m), quote(r)])
+            kw = kw.replace(' ', '%20')
             if year_since or year_to:
                 url = make_url(kw, year_since, year_to)
                 search_query = scholarly.search_pubs_custom_url(url)
@@ -79,8 +81,6 @@ for e in tqdm(energy_terms):
                 search_query = scholarly.search_pubs_query(kw)
             i = 0
             while i < int(result_items):
-                # pause = random.uniform(0., 0.5)
-                # time.sleep(pause)
                 try:
                     res = next(search_query)
                     i += 1
@@ -95,4 +95,7 @@ for e in tqdm(energy_terms):
                 except StopIteration:
                     break
     results_pd = pd.DataFrame.from_dict(results)
-    results_pd.to_csv(f'./{e}.csv', index=False)
+    results_pd.to_csv(f'./since2015/{e}.csv', index=False)
+    # if not e == energy_terms[-1]:
+    #     pause = random.uniform(300, 600)
+    #     time.sleep(pause)
